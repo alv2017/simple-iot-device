fb83acae33b2a06e37f9829f98625ce1f206544e
alv2017 <v.alishauskaite@gmail.com>
2022-12-27 19:51:36 +0200

    Simple IOT Device project is added to Git.

---
+++ tests/test_simple_iot_device.py
@@ -0,0 +1,32 @@
+import pytest
+from simple_iot_device import SimpleIOTDevice
+
+
+@pytest.fixture()
+def simple_test_device():
+    device_name = "SIMPLE-TEST"
+    device = SimpleIOTDevice(device_name)
+    return device
+
+
+class TestSimpleIOTDevice:
+    def test_create_simple_iot_device(self):
+        device_name = "SDM-125200"
+        device = SimpleIOTDevice(device_name)
+        assert isinstance(device, SimpleIOTDevice)
+        assert device.name == device_name
+        assert device.description == "Simple IOT Device"
+
+    def test_create_simple_iot_device_with_custom_description(self):
+        device_name = "SDM-125500"
+        device_description = "Simple IOT Device with Custom Description"
+        device = SimpleIOTDevice(device_name, device_description)
+        assert isinstance(device, SimpleIOTDevice)
+        assert device.name == device_name
+        assert device.description == device_description
+
+    def test_device_making_measurement_returns_non_empty_dictionary(self, simple_test_device):
+        device = simple_test_device
+        m = device.make_measurement()
+        assert isinstance(m, dict)
+        assert m
